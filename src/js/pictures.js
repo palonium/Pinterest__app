89a280c9fb24e8bc350592bcde6c7786f84b557d
polonium <polia.berdnik@gmail.com>
2023-09-18 23:43:17 +0300

    save pictures to ls

--- src/js/pictures.js
+++ src/js/pictures.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { evListener } from './modal-windows.js';
-
+export { photosData };
 export function addToPage(id, src, alt, tags, avatarSrc) {
     const pictureItem = document.createElement('div');
     pictureItem.className = 'pictures-item';
@@ -53,63 +53,53 @@ export function addToPage(id, src, alt, tags, avatarSrc) {
     pictureComplain.innerHTML = 'Пожаловаться';
     pictureMenu.append(pictureComplain);
     picBox.append(pictureMenu);
-
-
 }
 
+let photosData = getPhotosFromLocalStorage();
 
-export const photosData = [
-    { src: 'img/1.png', alt: '1', tags: '#дракон', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/2.jpg', alt: '2', tags: '#парк', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/3.jpg', alt: '2', tags: '#бойцовскийклуб', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/4.jpg', alt: '2', tags: '#кот', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/5.jpg', alt: '2', tags: '#вода', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/6.jpg', alt: '2', tags: '#медузы', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/7.jpg', alt: '2', tags: '#абстракция', avatarSrc: 'img/avatar.jpg' },
-    { src: 'img/8.jpg', alt: '2', tags: '#шрек', avatarSrc: 'img/avatar.jpg' },
-];
+if (photosData.length === 0) {
+    photosData = [
+        { src: 'img/1.png', alt: '1', tags: '#дракон', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/2.jpg', alt: '2', tags: '#парк', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/3.jpg', alt: '2', tags: '#бойцовскийклуб', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/4.jpg', alt: '2', tags: '#кот', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/5.jpg', alt: '2', tags: '#вода', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/6.jpg', alt: '2', tags: '#медузы', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/7.jpg', alt: '2', tags: '#абстракция', avatarSrc: 'img/avatar.jpg' },
+        { src: 'img/8.jpg', alt: '2', tags: '#шрек', avatarSrc: 'img/avatar.jpg' },
 
-for (let i = 0; i < photosData.length; i++) {
-    const photo = photosData[i];
-    addToPage(
-        `pic${i + 1}`,
-        photo.src,
-        photo.alt,
-        photo.tags,
-        photo.avatarSrc
-    );
+    ];
+
+    savePhotosToLocalStorage();
 }
 
-const searchInput = document.querySelector('.header__search');
-
-searchInput.addEventListener('input', function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    const picturesContainer = document.querySelector('.pictures');
-
-    while (picturesContainer.firstChild) {
-        picturesContainer.removeChild(picturesContainer.firstChild);
-    }
-
+function displayPhotos() {
     for (let i = 0; i < photosData.length; i++) {
         const photo = photosData[i];
-        const tagsLowerCase = photo.tags.toLowerCase();
-
-        if (tagsLowerCase.includes(searchTerm)) {
-            addToPage(
-                `pic${i + 1}`,
-                photo.src,
-                photo.alt,
-                photo.tags,
-                photo.avatarSrc
-            );
-        }
+        addToPage(
+            `pic${i + 1}`,
+            photo.src,
+            photo.alt,
+            photo.tags,
+            photo.avatarSrc
+        );
     }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    displayPhotos();
+    evListener();
 });
 
+function savePhotosToLocalStorage() {
+    localStorage.setItem('photosData', JSON.stringify(photosData));
+}
+function getPhotosFromLocalStorage() {
+    const photosDataStr = localStorage.getItem('photosData');
+    return photosDataStr ? JSON.parse(photosDataStr) : [];
+}
 
-function addPhotoFromDevice() {
-    const fileInput = document.getElementById('fileInput');
-    const hashTagInput = document.getElementById('hashTagInput');
+function addPhotoFromDevice(fileInput, hashTagInput) {
     const file = fileInput.files[0];
     const hashTag = hashTagInput.value;
 
@@ -144,15 +134,18 @@ function addPhotoFromDevice() {
                 newPicBox.querySelector('.picture__menu').classList.remove('visible');
             });
 
+            savePhotosToLocalStorage(); 
         };
     }
-    addEventListenersToPictures();
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    const fileInput = document.getElementById('fileInput');
+    const hashTagInput = document.getElementById('hashTagInput');
     const uploadButton = document.getElementById('uploadButton');
-    uploadButton.addEventListener('click', addPhotoFromDevice);
-});
 
-evListener();
+    uploadButton.addEventListener('click', function () {
+        addPhotoFromDevice(fileInput, hashTagInput);
+    });
+});
